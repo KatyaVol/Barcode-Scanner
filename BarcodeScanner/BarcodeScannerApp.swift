12e9d74f582ec67dbfe757d8e0c95927de8006e7
Екатерина Волобуева <ekaterinavolobueva@MacBook-Pro-Ekaterina-2.local>
2025-10-13 12:23:35 +0300

    Initial commit

---
+++ BarcodeScanner/BarcodeScannerApp.swift
@@ -0,0 +1,17 @@
+//
+//  BarcodeScannerApp.swift
+//  BarcodeScanner
+//
+//  Created by Ekaterina Volobueva on 13.10.2025.
+//
+
+import SwiftUI
+
+@main
+struct BarcodeScannerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            BarcodeScannerView()
+        }
+    }
+}
---
+++ BarcodeScanner/BarcodeScannerView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  BarcodeScanner
+//
+//  Created by Ekaterina Volobueva on 13.10.2025.
+//
+
+import SwiftUI
+
+struct BarcodeScannerView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    BarcodeScannerView()
+}
